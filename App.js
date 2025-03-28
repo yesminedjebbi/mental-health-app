6db6877f3f3e22f368218fe2255669808b382258
chayma095 <chaymabenchakir09@gmail.com>
2025-03-28 17:00:10 +0100

    Add frontend files

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import SignIn from './src/signin';  // Correct path to the SignIn component
 export default function App() {
   return (
     <View style={styles.container}>
-      <SignIn />  {/* Use the SignIn component here */}
+      <SignIn />  {/* Useee the SignIn component here */}
       <StatusBar style="auto" />
     </View>
   );
